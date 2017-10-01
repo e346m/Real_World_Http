386dc1d4fe799d3244e348052819ae3aeec402ed
eiji mishiro <eiji346g@gmail.com>
2017-10-01 20:07:34 +0900

    http1.0 semantics

--- Chapter_1/echo_server.go
+++ Chapter_1/echo_server.go
@@ -5,6 +5,8 @@ import(
   "log"
   "net/http"
   "net/http/httputil"
+  "io/ioutil"
+  "github.com/k0kubun/pp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +19,45 @@ func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "<html><body>hello</body></html>\n")
 }
 
+func handlerCookie(w http.ResponseWriter, r *http.Request) {
+  dump, err := httputil.DumpRequest(r, true)
+  if err != nil {
+    http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+    return
+  }
+  w.Header().Add("Set-Cookie", "VISIT=TRUE")
+  if _,  ok := r.Header["Cookie"]; ok {
+    fmt.Println(string(dump))
+    fmt.Fprint(w, "<html><body>second time</body></html>\n")
+  } else {
+    fmt.Println(string(dump))
+    fmt.Fprint(w, "<html><body>hello</body></html>\n")
+  }
+}
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+  pp.Printf("URL: %s\n", r.URL.String())
+  pp.Printf("Query: %v\n", r.URL.Query())
+  pp.Printf("Proto: %s\n", r.Proto)
+  pp.Printf("Method: %s\n", r.Method)
+  pp.Printf("Header: %v\n", r.Header)
+  defer r.Body.Close()
+  body, _ := ioutil.ReadAll(r.Body)
+  fmt.Printf("--body--\n%s\n", string(body))
+  if _, ok := r.Header["Authorization"]; !ok {
+    w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone",
+      nonce="TgLc25U2BQA=f510a2780473e18e6589be702c2b04afd", algorithm=MD5, qop="auth"`)
+    w.WriteHeader(http.StatusUnauthorized)
+  } else {
+    fmt.Fprintf(w, "<html><body>secret page</body><html>\n")
+  }
+}
+
 func main() {
   var httpServer http.Server
   http.HandleFunc("/", handler)
+  http.HandleFunc("/cookie", handlerCookie)
+  http.HandleFunc("/digest", handlerDigest)
   log.Println("start http listening :18888")
   httpServer.Addr = ":18888"
   log.Println(httpServer.ListenAndServe())
